5a21ec96d85b78b07ad261e56f629a2e5489a069
iamapinan <iamapinan@gmail.com>
2020-03-18 14:28:53 +0000

    initial

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php return array (
+  'debug' => false,
+  'database' => array (
+    'driver' => 'mysql',
+    'host' => 'opendata_db_1',
+    'database' => 'opendata_db',
+    'username' => 'root',
+    'password' => 'v4boyomN',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'io_',
+    'strict' => false,
+  ),
+  'url' => 'https://opendata.iotech.co.th',
+  'paths' => array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
